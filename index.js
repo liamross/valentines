9e85da6506bf5f187f97dd636a96f1518c1b55f1
Liam Ross <liamross.code@gmail.com>
2020-02-10 22:41:25 -0800

    Store value if valid

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 import CryptoJS from "crypto-js";
 import cypher from "./cypher";
 
+const storage = "password";
+const delimiter = "|||";
+
 const meet = document.getElementById("meet");
 const lung = document.getElementById("lung");
 const date = document.getElementById("date");
 
 const text = document.getElementById("text");
 
+function storePassword(password) {
+  setTimeout(() => {
+    localStorage.setItem(storage, password);
+  });
+}
+
+function setValuesFromStoredPassword() {
+  const stored = localStorage.getItem(storage);
+  if (!stored) return;
+
+  const values = stored.split(delimiter);
+  meet.value = values[0] || "";
+  lung.value = values[1] || "";
+  date.value = values[2] || "";
+}
+
 function decrypt(...passwordParts) {
   const password = passwordParts.join(" ");
   if (!password.trim()) return (text.innerHTML = "");
+
   const bytes = CryptoJS.AES.decrypt(cypher, password);
   let plaintext;
   try {
-    text.classList.add("valid");
     plaintext = bytes.toString(CryptoJS.enc.Utf8);
+    text.classList.add("valid");
+    storePassword(passwordParts.join(delimiter));
   } catch {
     text.classList.remove("valid");
     plaintext = bytes.toString();
@@ -33,5 +54,7 @@ function onChange() {
 meet.addEventListener("input", onChange);
 lung.addEventListener("input", onChange);
 date.addEventListener("input", onChange);
+
+setValuesFromStoredPassword();
 
 onChange();
